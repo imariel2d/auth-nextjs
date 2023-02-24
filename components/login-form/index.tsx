24ac884b4b7a4af0ff72d5c07f8a0129e6d2e2c9
Ariel Plascencia <imariel2d@gmail.com>
2023-02-24 12:31:52 -0700

    Initial commit

---
+++ components/login-form/index.tsx
@@ -0,0 +1,9 @@
+export const LoginForm = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Email"/>
+      <input type="text" placeholder="Password"/>
+      <button>Login</button>
+    </form>
+  )
+};
